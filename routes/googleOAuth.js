69c70a14b5a8a6c12b78991e5d90ccc104a1b470
dhyeygit123 <83972456+dhyeygit123@users.noreply.github.com>
2025-08-20 01:31:07 +0530

    Update googleOAuth.js

--- routes/googleOAuth.js
+++ routes/googleOAuth.js
@@ -18,7 +18,7 @@ function newOAuthClient() {
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URI
+    "https://youtube-vidcollab-server.onrender.com/api/google/oauth2callback"
   );
 }
 
