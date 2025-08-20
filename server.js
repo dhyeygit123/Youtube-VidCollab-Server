f0ec394179372f3cc68720dd23ecc856d9c94c0b
Dhyey <dhyey.p@ahduni.edu.in>
2025-08-20 17:39:34 +0530

    Making server changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const videoRoutes = require('./routes/video');
 const teamRoutes = require('./routes/team');
 const googleRoutes = require('./routes/googleOAuth'); 
 const mongoose = require("mongoose");
-const DbConfig = require("./config/db")
+const DbConfig = require("./config/db");
 const app = express();
 require("dotenv").config();
 
@@ -13,7 +13,7 @@ DbConfig();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+  
 app.get('/', (req, res) => {
   res.send('Welcome to the YouTube Collab Platform API 🚀');
 });
@@ -24,15 +24,5 @@ app.use('/api/video', videoRoutes);
 app.use('/api/team', teamRoutes);
 app.use('/api/google', googleRoutes); 
 
-
-app.get("/api/test", (req, res) => {
-  res.json({ ok: true, source: "vercel" });
-});
-
-// Start server
-if (require.main === module) {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log("Running locally on port " + PORT));
-}
-
+// Export the app for Vercel
 module.exports = app;
