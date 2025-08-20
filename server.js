46c10169929d8704237127cf0bedcd0f56a25006
Dhyey <dhyey.p@ahduni.edu.in>
2025-08-20 17:11:28 +0530

    Vercel Upload

--- server.js
+++ server.js
@@ -24,5 +24,7 @@ app.use('/api/video', videoRoutes);
 app.use('/api/team', teamRoutes);
 app.use('/api/google', googleRoutes); 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
